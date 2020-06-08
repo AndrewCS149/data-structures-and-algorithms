b4191d0b52d9a51938f17d707269a886e2fd4a11
AndrewCS149 <andrewcs149@gmail.com>
2020-06-07 17:48:53 -0700

    Complete challenge 6.

--- code-challenges/array-and-string-methods/challenges-05.test.js
+++ code-challenges/array-and-string-methods/challenges-05.test.js
@@ -173,7 +173,12 @@ Return a new array containing just the verbs. For example, ['Mix until evenly di
 
 const stepActions = (recipe) => {
   let result = [];
-  // Solution code here...
+
+  recipe.steps.forEach(val => {
+    let idx = val.indexOf(' ');
+    result.push(val.slice(0, idx));
+  });
+
   return result;
 };
 
@@ -304,7 +309,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should return a list of recipe steps', () => {
     expect(stepActions(gruffaloCrumble)).toStrictEqual(['Pre-heat', 'De-prickle', 'Sprinkle', 'Mix', 'Grease', 'Combine', 'Fold', 'Spread', 'Bake']);
     expect(stepActions(gruffaloCrumble).length).toStrictEqual(9);
