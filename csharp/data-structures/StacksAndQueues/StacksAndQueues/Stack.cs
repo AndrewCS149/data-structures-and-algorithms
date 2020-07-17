c9befc7e51de9f4e6bbb2622243ecf15cbd0b44d
Andrew Smith <andrewcs149@gmail.com>
2020-07-17 14:29:12 -0700

    Create Peek().

--- csharp/data-structures/StacksAndQueues/StacksAndQueues/Stack.cs
+++ csharp/data-structures/StacksAndQueues/StacksAndQueues/Stack.cs
@@ -8,11 +8,26 @@ namespace StacksAndQueues
     {
         public Node Top { get; set; }
 
+        /// <summary>
+        /// Pushes a new node to the top of the stack
+        /// </summary>
+        /// <param name="value">Value of the node to push</param>
         public void Push(string value)
         {
             Node node = new Node(value);
             node.Next = Top;
             Top = node;
+        }
+
+        /// <summary>
+        /// Removes node from top of stack
+        /// </summary>
+        /// <returns>Returns top node's string value</returns>
+        public string Peek()
+        {
+            if (Top.Value == null)
+                throw new NullReferenceException("Stack is empty");
+            return Top.Value;
         }
     }
 }
--- csharp/data-structures/StacksAndQueues/StacksAndQueuesTests/UnitTest1.cs
+++ csharp/data-structures/StacksAndQueues/StacksAndQueuesTests/UnitTest1.cs
@@ -6,14 +6,33 @@ namespace StacksAndQueusTests
 {
     public class UnitTest1
     {
+        // Test Push()
         [Fact]
         public void CanPushOnStack()
         {
+            // arrange
             Stack stack = new Stack();
 
+            // act
             stack.Push("candy cane");
 
+            // assert
             Assert.Equal("candy cane", stack.Top.Value);
+        }
+
+        //// Test Peek()
+        [Fact]
+        public void CanPeekTopAndReturnItsValue()
+        {
+            // arrange
+            Stack stack = new Stack();
+            stack.Push("Dog");
+
+            // act
+            string actual = stack.Peek();
+
+            // assert
+            Assert.Equal("Dog", actual);
         }
     }
 }
