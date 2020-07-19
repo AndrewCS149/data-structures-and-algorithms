69f03c2c3c555b7cb519f04274c3595675fb598c
Andrew Smith <andrewcs149@gmail.com>
2020-07-19 02:02:14 -0700

    Add Dequeue().

--- csharp/data-structures/StacksAndQueues/StacksAndQueues/Queue.cs
+++ csharp/data-structures/StacksAndQueues/StacksAndQueues/Queue.cs
@@ -14,6 +14,10 @@ namespace StacksAndQueues
             Rear = Front;
         }
 
+        /// <summary>
+        /// Enqueue a Node into a queue
+        /// </summary>
+        /// <param name="val">String value to enqueue</param>
         public void Enqueue(string val)
         {
             Node node = new Node(val);
@@ -28,6 +32,22 @@ namespace StacksAndQueues
                 Rear.Next = node;
                 Rear = node;
             }
+        }
+
+        /// <summary>
+        /// Dequeue the front node from the queue
+        /// </summary>
+        /// <returns>Returns the front nodes value</returns>
+        public string Dequeue()
+        {
+            // if stack is empty
+            if (Front == null)
+                throw new NullReferenceException("Queue is empty");
+
+            Node temp = Front;
+            Front = Front.Next;
+
+            return temp.Value;
         }
     }
 }
--- csharp/data-structures/StacksAndQueues/StacksAndQueuesTests/QueueTests.cs
+++ csharp/data-structures/StacksAndQueues/StacksAndQueuesTests/QueueTests.cs
@@ -9,18 +9,60 @@ namespace StacksAndQueuesTests
     public class QueueTests
     {
 
+        // Test Enqueue()
         [Fact]
-        public void CanEqueueIntoQueue()
+        public void CanEnqueueIntoQueue()
         {
             // arrange 
             Queue queue = new Queue();
 
             // act
             queue.Enqueue("Courtney");
-            queue.Enqueue("Andrew");
 
             // assert
             Assert.Equal("Courtney", queue.Front.Value);
+        }
+
+        // Test Enqueue()
+        [Fact]
+        public void CanEnqueueMultipleNodesToQueue()
+        {
+            // arrange
+            Queue queue = new Queue();
+
+            // act
+            queue.Enqueue("Dog");
+            queue.Enqueue("Cat");
+            queue.Enqueue("Mouse");
+
+            string front = queue.Front.Value;
+            string middle = queue.Front.Next.Value;
+            string rear = queue.Rear.Value;
+
+            bool fullQueue = false;
+            if (front == "Dog" && middle == "Cat" && rear == "Mouse")
+                fullQueue = true;
+
+            // assert
+            Assert.True(fullQueue);
+        }
+
+        // Test Dequeue() 
+        [Fact]
+        public void CanDequeueAndReturnTheFrontValue()
+        {
+            // arrange
+            Queue queue = new Queue();
+
+            // act
+            queue.Enqueue("Dog");
+            queue.Enqueue("Cat");
+            queue.Enqueue("Mouse");
+
+            string frontValue = queue.Dequeue();
+
+            // assert
+            Assert.Equal("Dog", frontValue);
         }
     }
 }
