59591fac338b7b53af9c69e91ab42596d63c42f6
Andrew Smith <andrewcs149@gmail.com>
2020-07-20 21:45:50 -0700

    Restructure.

---
+++ csharp/code-challenges/QueuesWithStacks/QueuesWithStacks/PseudoQueue.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace QueuesWithStacks
+{
+    public class PseudoQueue
+    {
+    }
+}
---
+++ csharp/code-challenges/QueuesWithStacks/QueuesWithStacksTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace QueuesWithStacksTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
