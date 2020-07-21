24cedc005ab16d0b709852cb06d8046375080d9a
Andrew Smith <andrewcs149@gmail.com>
2020-07-20 20:41:25 -0700

    Add PseudoQueue class and intialize stack, Enqueue() and Dequeue(). Add test project.

--- csharp/data-structures/QueueWithStacks/QueueWithStacks/PseudoQueue.cs
+++ csharp/data-structures/QueueWithStacks/QueueWithStacks/PseudoQueue.cs
@@ -1,23 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Collections;
 
 namespace QueueWithStacks
 {
-    class PseudoQueue
+    public class PseudoQueue
     {
+        public Stack Stack { get; set; }
 
+        public PseudoQueue(Stack stack)
+        {
+            Stack = stack;
+        }
 
+        /// <summary>
+        /// Enqueues a value onto the PseudoQueue
+        /// </summary>
+        /// <param name="val">The int value to enqueue</param>
         public void Enqueue(int val)
         {
+            Stack.Push(val);
 
         }
 
+        /// <summary>
+        /// Dequeues the front value from the PseudoQueue
+        /// </summary>
+        /// <returns>Returns the dequeued int value</returns>
         public int Dequeue()
         {
 
             return -1;
         }
-
     }
 }
