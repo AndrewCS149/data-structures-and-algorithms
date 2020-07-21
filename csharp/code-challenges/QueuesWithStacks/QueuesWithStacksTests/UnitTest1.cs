1d90dc00dc314dd0edd5e3065eee5d17b88eafb8
Andrew Smith <andrewcs149@gmail.com>
2020-07-20 22:19:51 -0700

    Add Enqueue(), Dequeue() and add unit tests.

--- csharp/code-challenges/QueuesWithStacks/QueuesWithStacksTests/UnitTest1.cs
+++ csharp/code-challenges/QueuesWithStacks/QueuesWithStacksTests/UnitTest1.cs
@@ -1,14 +1,46 @@
+using QueuesWithStacks;
 using System;
 using Xunit;
+using static QueuesWithStacks.PseudoQueue;
 
 namespace QueuesWithStacksTests
 {
     public class UnitTest1
     {
+        // Test Enqueue
         [Fact]
-        public void Test1()
+        public void CanEnqueueIntoPseudoQueue()
         {
+            // arrange
+            PseudoQueue myStack = new PseudoQueue();
+            myStack.Enqueue("Dog");
+            myStack.Enqueue("Cat");
+            myStack.Enqueue("Mouse");
+            myStack.Enqueue("Bear");
 
+            // act
+            string returnedValue = myStack.Peek();
+
+            // assert
+            Assert.Equal("Dog", returnedValue);
+        }
+
+        // Test Dequeue
+        [Fact]
+        public void CanDequeueFromPseudoQueue()
+        {
+            // arrange
+            PseudoQueue myStack = new PseudoQueue();
+            myStack.Enqueue("Dog");
+            myStack.Enqueue("Cat");
+            myStack.Enqueue("Mouse");
+            myStack.Enqueue("Bear");
+
+            // act
+            string returnedValue = myStack.Dequeue();
+
+            // assert
+            Assert.Equal("Bear", returnedValue);
         }
     }
 }
