e09e7abd06a60af591684ee2bc60af4fd29e4f06
Andrew Smith <andrewcs149@gmail.com>
2020-07-20 19:37:20 -0700

    Initial Commit.

---
+++ csharp/data-structures/QueueWithStacks/QueueWithStacks/PseudoQueue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueWithStacks
+{
+    class PseudoQueue
+    {
+
+
+        public void Enqueue(int val)
+        {
+
+        }
+
+        public int Dequeue()
+        {
+
+            return -1;
+        }
+
+    }
+}
