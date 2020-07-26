53163dc40a5965e8e7f5354391cacdfd7c847fe2
Andrew Smith <andrewcs149@gmail.com>
2020-07-26 16:12:56 -0700

    Create binary search tree constructors.

--- csharp/data-structures/Trees/Trees/BinarySearchTree.cs
+++ csharp/data-structures/Trees/Trees/BinarySearchTree.cs
@@ -4,7 +4,34 @@ using System.Text;
 
 namespace Trees
 {
-    class BinarySearchTree
+    public class BinarySearchTree
     {
+        public Node<int> Root { get; set; }
+
+        /// <summary>
+        /// Constructor for empty binary search tree
+        /// </summary>
+        public BinarySearchTree()
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor for binary search tree with root node
+        /// </summary>
+        /// <param name="value">The root node's value</param>
+        public BinarySearchTree(int value)
+        {
+            Node<int> root = new Node<int>(value);
+            Root = root;
+        }
+
+        public void Insert(int value)
+        {
+            if(value <= Root.Value)
+            {
+
+            }
+        }
     }
 }
